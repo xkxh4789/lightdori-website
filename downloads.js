31c14ffe766870042fb5c0686d766057459c1f64
Lee <xkxh4789@gmail.com>
2026-07-30 17:00:36 +0900

    Update downloads.js

--- downloads.js
+++ downloads.js
@@ -16,6 +16,6 @@ window.LIGHTDORI_DOWNLOADS = {
   // Paste the Windows .exe download URL here (must start with https://)
   windows: "https://github.com/xkxh4789/Lightdori-release/releases/download/v0.1.0/LightDori_Setup_1.0_win64.exe",
   // Paste the macOS .dmg / .zip download URL here
-  mac: "",
+  mac: "https://github.com/xkxh4789/Lightdori-release/releases/download/v0.1.0/LightDori_Setup_1.0.0.dmg",
   note: "",
 };
